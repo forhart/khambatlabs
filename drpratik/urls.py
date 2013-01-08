fb74946395e2679b16a468c693afc66033a2bfce
SagarD <foraart@gmail.com>
2013-01-08 16:30:47 +0530

    Too old.. .to remember what is done.. .

--- drpratik/urls.py
+++ drpratik/urls.py
@@ -1,17 +1,25 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import list_detail
+
+from drpratik.khambatlabs.models import Patient
+
+patient_info = {
+    'queryset':Patient.objects.all(),
+}
+
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'drpratik.views.home', name='home'),
     # url(r'^drpratik/', include('drpratik.foo.urls')),
-
+    (r'^patients/$',list_detail.object_list,patient_info),                   
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
